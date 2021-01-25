a08010b9771efb978bfa6e8832a20a8be6a1cf0f
L0Angel98 <75176852+L0Angel98@users.noreply.github.com>
2021-01-24 22:12:01 -0600

    Feature 1.3

---
+++ java/main.js
@@ -0,0 +1,30 @@
+var images = [];
+images[0] = "../images/headerImg/header1.jpg";
+images[1] = "../images/headerImg/header2.jpg";
+images[2] = "../images/headerImg/header3.jpg";
+images[3] = "../images/headerImg/header4.jpg";
+images[4] = "../images/headerImg/header5.jpg";
+
+var titles = [];
+
+titles[0] = "THERE IS NO PLANET B";
+titles[1] = "NATURE IS OUR ONLY CHANCE";
+titles[2] = "BIOLOGY AS TECHNOLOGY";
+titles[3] = "INDUSTRY 5.0";
+titles[4] = "WE GROW BIOMATERIALS";
+
+var description = [];
+
+description[0] = "We face one great challenge today as a species, planetary health. What we do in the next 10 years will determine the future of mankind.";
+description[1] = "Life, which we have marginated to the brink of extinction, is actually our best collaborator for the future.";
+description[2] = "All life forms are metabolic processes, we use this processes as industrial production platforms";
+description[3] = "We are on the verge of a BioFab revolution that will fundamentally transform the way food, chemicals, advanced materials, medicine and fuel is created.";
+description[4] = "We are a BioFabrication Mexican company developing novel materials out of life itself. We relay in the building blocks of life to manufacture the next generation of materials.";
+
+
+var datos = document.getElementById("titles1");
+datos.innerHTML = `
+<h1 id= th>${titles[0]}</h1>
+<p id=pheader>${description[0]}</p>
+  `;
+//document.body.style.background = "url('../images/headerImg/header1.jpg')";
